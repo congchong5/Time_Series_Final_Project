1c090b092bee06ca86b396b2db14dad4fc918aa0
Kuan-Wei Chen <36137847+congchong5@users.noreply.github.com>
2018-05-12 15:22:45 -0400

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -15,6 +15,16 @@ from statsmodels.tsa import seasonal
 import seaborn as sns
 from datetime import datetime
 
+def stationary_test(timeseries):
+    # Perform Dickey-Fuller test
+    summary=adfuller(timeseries)
+    print('Results of Augmented Dickey-Fuller Test:')
+    print('Augmented Dickey-Fuller Statistic:%4.f'%summary[0])
+    print('p-value:%f'%summary[1])
+    print('Crititcal Values:')
+    for key,value in summary[4].items():
+        print('\t%s:%.4f' % (key, value))
+
 ## Detrend with time
 data = pd.read_excel("inflation.xlsx")
 data.columns = ['Inflation_Data']
